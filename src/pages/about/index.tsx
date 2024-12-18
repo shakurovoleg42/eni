28d561b8c92eb7652db95b2809b70fd5ddf84c04
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-12-18 17:58:35 +0500

    fxi about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -6,15 +6,34 @@ const About = () => {
   return (
     <Container className="mb-12">
       <Banner />
-      <div className="flex flex-col mt-12 gap-56 font-inter font-[400]">
+      <div className="flex flex-col mt-12 gap-40 font-inter font-[400] px-3">
         <div className="flex flex-col lg:flex-row gap-12">
-          <Image
-            src="/main-page-banner.jpeg"
-            width={520}
-            height={481}
-            alt="about-images"
-          />
-          <p className="text-[22.7px]">
+          <div className="flex flex-row gap-4">
+            <Image
+              src="/main-page-banner.jpeg"
+              width={520}
+              height={481}
+              alt="about-images"
+              className="rounded-[22px]"
+            />
+            <div className="hidden lg:flex flex-col gap-4">
+              <Image
+                src="/about-img-second.png"
+                width={250}
+                height={231.31}
+                alt="about-images"
+              />
+              <Image
+                src="/about-third.png"
+                width={250}
+                height={231.31}
+                alt="about-images"
+                className="rounded-[18px]"
+              />
+            </div>
+          </div>
+
+          <p className="text-[22.7px] max-w-[790px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation Lorem ipsum dolor sit
@@ -30,7 +49,7 @@ const About = () => {
           </p>
         </div>
         <div className="flex flex-col lg:flex-row gap-12">
-          <p className="text-[22.7px]">
+          <p className="text-[22.7px] max-w-[1060px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation Lorem ipsum dolor sit
@@ -45,10 +64,11 @@ const About = () => {
             incididunt ut labore et dolore magna aliqua.
           </p>
           <Image
-            src="/main-page-banner.jpeg"
+            src="/about-fourth.png"
             width={520}
             height={481}
             alt="about-images"
+            className="rounded-[22px]"
           />
         </div>
       </div>
